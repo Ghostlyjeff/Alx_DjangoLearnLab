d1c6b01ad2894b28ee47849c327bc9ad31341d10
Ghostlyjeff <odifirijide1234@gmail.com>
2025-03-02 18:06:49 +0100

    new

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library  # Ensure Library is imported
 
-urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-]
+def list_books(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"  # Ensure this template exists
+    context_object_name = "library"
+
