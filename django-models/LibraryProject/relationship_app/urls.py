d862bea15497f8e272f79f0e513d7eca7bde5808
Ghostlyjeff <odifirijide1234@gmail.com>
2025-03-08 09:55:50 +0100

    almost failed this

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path('librarian-dashboard/', librarian_view, name='librarian_dashboard'),
     path('member-dashboard/', member_view, name='member_dashboard'),
 ]
+from django.urls import path
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+]
