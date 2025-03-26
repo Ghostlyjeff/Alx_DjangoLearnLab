a3ca4d090d440fb2ef478909c36ac1643954c928
Ghostlyjeff <odifirijide1234@gmail.com>
2025-03-26 19:02:13 +0100

    initialize login information

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,3 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
 ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views  # Import custom views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+]
