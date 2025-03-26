4a31d3313d00273faeeeb48cb4866e751349a2b9
Ghostlyjeff <odifirijide1234@gmail.com>
2025-03-26 19:32:31 +0100

    all new add ons

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,7 +5,8 @@ def home(request):
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -19,5 +20,20 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'blog/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'blog/edit_profile.html', {'form': form})
 
 # Create your views here.
